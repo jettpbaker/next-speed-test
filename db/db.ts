8b6a74df1406e055d59c182f1e5e75a542925655
Jett Baker <jett.p.baker@gmail.com>
2025-08-01 20:02:50 +1000

    removed the DB config

--- db/db.ts
+++ db/db.ts
@@ -5,10 +5,6 @@ import * as schema from './schema'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  max: 1,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 })
 
 const db = drizzle(pool, { schema })
