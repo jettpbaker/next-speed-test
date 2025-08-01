4a327aaf09b38acb32fe756ced835224f2e68e84
Jett Baker <jettbaker@Jetts-MacBook-Pro.local>
2025-08-01 17:30:38 +1000

    wrap text content on post page

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -3,8 +3,9 @@ import Link from 'next/link'
 export default function Post() {
   return (
     <div>
-      This is an example of a post page. I'm curious how long this takes to
-      serve all the way to Brisbane!
+      {
+        "This is an example of a post page. I'm curious how long this takes to serve all the way to Brisbane!"
+      }
       <br />
       <Link href="/">Home</Link>
     </div>
